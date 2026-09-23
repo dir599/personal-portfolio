f0456086bd592e794794e2c071a80b310a6d4518
Dirag Kasula <190046038+dir599@users.noreply.github.com>
2026-09-23 20:40:28 +0545

    feat: add background animations and main profile card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,106 @@
-import React from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
+import pp from "./assets/pp.jpeg"
+import { motion } from "framer-motion";
+import {
+  Clock,
+  Trophy,
+  User,
+  BriefcaseBusiness,
+  ExternalLink,
+  Mail,
+} from "lucide-react";
+import {
+  personalInfo,
+  projects,
+  socialLinks,
+  experiences,
+  tools,
+  goals,
+  achievements,
+  skills,
+  certificates,
+  animatedBlobs,
+} from "./constants/data.jsx";
 
 const App = () => {
-  return (
-    <div>
-      <h1>This is app.</h1>
-    </div>
-  )
-}
+  const [time, setTime] = useState();
+  const [mounted, setMounted] = useState(false);
 
-export default App
+  useEffect(() => {
+    setMounted(true);
+    setTime(new Date().toLocaleDateString());
+    const timer = setInterval(() => {
+      setTime(new Date().toLocaleDateString());
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.25,
+        ease: "easeOut",
+        duration: 0.7,
+      },
+    },
+  };
+
+  const itemVariants = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.6,
+        ease: [0.22, 1, 0.36, 1],
+      },
+    },
+  };
+
+  return (
+    <>
+      <div className="relative min-h-screen flex justify-center items-center bg-[#0a0a0a] text-white p-4 md:py-14 font-mono overflow-hidden">
+        {animatedBlobs.map((blob, i) => (
+          <motion.div
+            key={i}
+            className={`absolute rounded-full ${blob.className}`}
+            animate={blob.animate}
+            transition={{
+              duration: blob.duration,
+              ease: "easeInOut",
+              repeat: Infinity,
+              repeatType: "mirror",
+            }}
+          ></motion.div>
+        ))}
+
+        <motion.main
+          variants={containerVariants}
+          initial="hidden"
+          animate={mounted ? "show" : "hidden"}
+          className="main-grid"
+        >
+          <motion.div
+            variants={itemVariants}
+            className="card card-cyan md:col-span-1 
+            row-span-3 flex flex-col justify-center gap-3"
+          >
+            <img src={pp} alt="photo" className="w-[70px] h-[70px]
+            rounded-full object-cover" />
+            <h2>
+              <User size={22} className="text-violet-400"/>
+              <p>{personalInfo.name}</p>
+            </h2>
+
+            {/* project card */}
+          </motion.div>
+        </motion.main>
+      </div>
+    </>
+  );
+};
+
+export default App;
